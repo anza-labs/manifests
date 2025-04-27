99bbc5429d8cdd4d02e68af9d73dd9e72560def7
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2025-04-27 15:12:24 +0200

    ci: debugging issues
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- hack/test.sh
+++ hack/test.sh
@@ -6,8 +6,9 @@ declare -a failed_builds
 # Function to find and process kustomization.yaml files
 process_kustomizations() {
     local dir="$1"
+    echo "Root dir: $dir, pwd: $(pwd)"
     for file in $(find "$dir" -type f -name "kustomization.yaml"); do
-        echo "Processing: $file"
+        echo "Processing: $file in $(dirname "$file")"
         if ! kustomize build "$(dirname "$file")" > /dev/null; then
             echo "Failed: $file"
             failed_builds+=("$file")
